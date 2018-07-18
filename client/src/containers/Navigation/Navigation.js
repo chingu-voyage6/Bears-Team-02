9dc51a1c98b04706a35445335b658f89015f55aa
Christian Zenaty <cazenaty@yahoo.com>
2018-07-18 15:25:05 -0700

    Quick fix for navbar responsiveness issue

--- client/src/containers/Navigation/Navigation.js
+++ client/src/containers/Navigation/Navigation.js
@@ -13,6 +13,30 @@ const linksOut = [
   { name: "Sign Up", url: "/signup", class: "navbar-item" },
   { name: "Log In", url: "/login", class: "navbar-item" }
 ];
+// TODO - Find cleaner way to handle navbar-burger toggle - classnames?
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener("click", () => {
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
+});
 
 class Navigation extends Component {
   componentDidMount() {
@@ -22,18 +46,31 @@ class Navigation extends Component {
   render() {
     return (
       <nav className="navbar is-info is-spaced">
-        <div className="navbar-brand">
-          <Link to="/" className="navbar-item">
-            FC
-          </Link>
-        </div>
-        <div className="navbar-menu">
-          <div className="navbar-end">
-            {this.props.authenticated ? (
-              <Links links={linksIn} />
-            ) : (
-              <Links links={linksOut} />
-            )}
+        <div className="container">
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item">
+              FC
+            </Link>
+            <a
+              role="button"
+              class="navbar-burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navMenu"
+            >
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+            </a>
+          </div>
+          <div id="navMenu" className="navbar-menu">
+            <div className="navbar-end">
+              {this.props.authenticated ? (
+                <Links links={linksIn} />
+              ) : (
+                <Links links={linksOut} />
+              )}
+            </div>
           </div>
         </div>
       </nav>
