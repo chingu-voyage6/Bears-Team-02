de23f3e5f901b28243101d2d58c5c112ce93ddbf
Christian Zenaty <cazenaty@yahoo.com>
2018-07-18 15:53:55 -0700

    Adding classnames and cleaned up navbar-burger handler

--- client/src/containers/Navigation/Navigation.js
+++ client/src/containers/Navigation/Navigation.js
@@ -3,6 +3,7 @@ import Links from "../../components/UI/Links";
 import "./Navigation.css";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import classNames from "classnames";
 
 const linksIn = [
   { name: "Connections", url: "/connect", class: "navbar-item" },
@@ -13,37 +14,25 @@ const linksOut = [
   { name: "Sign Up", url: "/signup", class: "navbar-item" },
   { name: "Log In", url: "/login", class: "navbar-item" }
 ];
-// TODO - Find cleaner way to handle navbar-burger toggle - classnames?
-document.addEventListener("DOMContentLoaded", () => {
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(
-    document.querySelectorAll(".navbar-burger"),
-    0
-  );
-
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-    // Add a click event on each of them
-    $navbarBurgers.forEach(el => {
-      el.addEventListener("click", () => {
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle("is-active");
-        $target.classList.toggle("is-active");
-      });
-    });
-  }
-});
 
 class Navigation extends Component {
+  state = { toggleBurger: false };
+
   componentDidMount() {
     console.log("CWD", this.props);
   }
 
+  handleBurgerClick = () => {
+    this.setState(prevState => ({ toggleBurger: !prevState.toggleBurger }));
+  };
+
   render() {
+    let navBurger = classNames("navbar-burger", {
+      "is-active": this.state.toggleBurger
+    });
+    let navMenu = classNames("navbar-menu", {
+      "is-active": this.state.toggleBurger
+    });
     return (
       <nav className="navbar is-info is-spaced">
         <div className="container">
@@ -53,17 +42,18 @@ class Navigation extends Component {
             </Link>
             <a
               role="button"
-              class="navbar-burger"
+              class={navBurger}
               aria-label="menu"
               aria-expanded="false"
               data-target="navMenu"
+              onClick={this.handleBurgerClick}
             >
               <span aria-hidden="true" />
               <span aria-hidden="true" />
               <span aria-hidden="true" />
             </a>
           </div>
-          <div id="navMenu" className="navbar-menu">
+          <div id="navMenu" className={navMenu}>
             <div className="navbar-end">
               {this.props.authenticated ? (
                 <Links links={linksIn} />
@@ -88,3 +78,28 @@ export default connect(
   null,
   { pure: true }
 )(Navigation);
+
+/* Saving for now - Handler for navBar Burger */
+// document.addEventListener("DOMContentLoaded", () => {
+//   // Get all "navbar-burger" elements
+//   const $navbarBurgers = Array.prototype.slice.call(
+//     document.querySelectorAll(".navbar-burger"),
+//     0
+//   );
+
+//   // Check if there are any navbar burgers
+//   if ($navbarBurgers.length > 0) {
+//     // Add a click event on each of them
+//     $navbarBurgers.forEach(el => {
+//       el.addEventListener("click", () => {
+//         // Get the target from the "data-target" attribute
+//         const target = el.dataset.target;
+//         const $target = document.getElementById(target);
+
+//         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+//         el.classList.toggle("is-active");
+//         $target.classList.toggle("is-active");
+//       });
+//     });
+//   }
+// });
